96250dc9754451f801275421e070a33fba3862c3
juanchobanano <juancepeda.gestion@gmail.com>
2022-05-17 01:12:29 +0300

    Driver modified

--- app/bot.py
+++ app/bot.py
@@ -65,14 +65,14 @@ def main(constants):
 
     # Crear driver.
     print("Open Chrome")
-    driver = webdriver.Chrome(executable_path="../test/chromedriver.exe")
+    #driver = webdriver.Chrome(executable_path="../test/chromedriver.exe")
     
-    #chrome_options = webdriver.ChromeOptions()
-    #chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    #chrome_options.add_argument("--headless")
-    #chrome_options.add_argument("--disable-dev-shm-usage")
-    #chrome_options.add_argument("--no-sandbox")
-    #driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     
     wait = WebDriverWait(driver, 10)
     #driver = webdriver.Remote(selenium_endpoint, desired_capabilities=DesiredCapabilities.FIREFOX)
